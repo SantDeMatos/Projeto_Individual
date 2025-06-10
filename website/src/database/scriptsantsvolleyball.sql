59ce8c6b1b4354ced780fa42aa6f47a60b76a51b
SantDeMatos <matheus.smatos@sptech.school>
2025-06-09 23:46:12 -0300

    adição de novos jogadores e clubes

--- website/src/database/scriptsantsvolleyball.sql
+++ website/src/database/scriptsantsvolleyball.sql
@@ -78,11 +78,35 @@ constraint pkallcl primary key (idfavoritar_clube, fkclube, fkusuario)
 );
 
 
-insert into jogadores values (default, 'Cachopa', 29, ' Volley Monza ', 'Atuando', '../assets/imgs/jogadores/cachopa.jpg');
-insert into jogadores values (default, 'Bruninho', 38, 'Renata/Campinas', 'Atuando', '../assets/imgs/jogadores/bruninho.webp');
+insert into jogadores values (default, 'Cachopa', 29, ' Volley Monza ', '../assets/imgs/jogadores/cachopa.jpg');
+insert into jogadores values (default, 'Bruninho', 38, 'Renata/Campinas', '../assets/imgs/jogadores/bruninho.webp');
 insert into clubes values (default, 'Sesi Bauru', 'Superliga', '2005', '../assets/imgs/clubes/sesi.png');
 insert into usuario values (default, 'Matheus', 'matheus@gmail.com', '96454-2424', 'matheussantana', 'Gerente');
 
-truncate quiz;
+insert into jogadores values (default, 'Lucão', 39, 'Sada Cruzeiro',  '../assets/imgs/jogadores/lucao.jpg'),
+(default, 'Lucarelli', 33, 'JTEKT Stings', '../assets/imgs/jogadores/lucarelli.webp'),
+(default, 'Macris', 36, 'Praia Clube', '../assets/imgs/jogadores/Macris.jpg'),
+(default, 'Rosamaria', 31, 'Denso Airybees',  '../assets/imgs/jogadores/rosamaria.jpg'),
+(default, 'Thales', 36, 'BOGDANKA LUK',  '../assets/imgs/jogadores/thales.jpg'),
+(default, 'Darlan', 22, 'Verona', '../assets/imgs/jogadores/darlan.jpg'),
+(default, 'Ana Cristina', 21, 'Fenerbahçe', '../assets/imgs/jogadores/anacristina.jpg'),
+(default, 'Lukas Bergmann', 21, 'Sesi Bauru',  '../assets/imgs/jogadores/lucasbergmann.jpg'),
+(default, 'Gabi', 31, 'Imoco Volley',  '../assets/imgs/jogadores/gabi.webp'),
+(default, 'Leal', 36, 'Bluenergy Piacenza',  '../assets/imgs/jogadores/leal.jpg'),
+(default, 'Júlia Bergmann', 24, 'Türk Hava',  '../assets/imgs/jogadores/juliabergmann.jpg'),
+(default, 'Alan Souza', 31, 'Sada Cruzeiro',  '../assets/imgs/jogadores/alansouza.jpg'),
+(default, 'Thaísa', 38, 'Minas Clube',  '../assets/imgs/jogadores/thaisa.jpg'),
+(default, 'Honorato', 28, 'Suwalki', '../assets/imgs/jogadores/honorato.jpg'),
+(default, 'Nyeme', 26, 'Minas Clube',  '../assets/imgs/jogadores/nyeme.jpg');
+
+insert into clubes values (default, 'Sada Cruzeiro', 'Superliga', '2005', '../assets/imgs/clubes/sada.png'),
+(default, 'Praia Clube', 'Superliga', '1989', '../assets/imgs/clubes/praia.png'),
+(default, 'Vôlei Renata', 'Superliga', '2010', '../assets/imgs/clubes/renata.jpg'),
+(default, 'Joinville Vôlei', 'Superliga', '2020', '../assets/imgs/clubes/joinville.jpg'),
+(default, 'Verona Volley', 'Superliga da Itália', '2001', '../assets/imgs/clubes/verona.png'),
+(default, 'JTEKT Stings', ' V.League', '1958', '../assets/imgs/clubes/jtekt.png'),
+(default, 'Osaka Bluteon', 'V.League', '1951', '../assets/imgs/clubes/osaka.png'),
+(default, 'Modena Volley', 'Superliga da Itália', '1996', '../assets/imgs/clubes/modena.png'),
+(default, 'Osasco Voleibol', 'Superliga', '2009', '../assets/imgs/clubes/osasco.png');
 
 
